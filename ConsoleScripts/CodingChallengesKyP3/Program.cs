ab79d4d67964b7675a54754f204e19881d77e591
Jason Ky <744169@student.dusd.net>
2024-10-17 14:58:10 -0700

    tried

--- ConsoleScripts/CodingChallengesKyP3/Program.cs
+++ ConsoleScripts/CodingChallengesKyP3/Program.cs
@@ -6,9 +6,10 @@ class CodingChallenges()
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Welcome to my Coding Challenge program. There will be a variety of different functions you will have to choose from. Please pick one.");
         ChooseFunction();
 
-        Console.WriteLine("Welcome to my Coding Challenge program. There will be a variety of different functions you will have to choose from. Please pick one.");
+       /* Console.WriteLine("Welcome to my Coding Challenge program. There will be a variety of different functions you will have to choose from. Please pick one.");
         Console.WriteLine("We are going to use the number adder called SUM. Please input two numbers for us to add. \n");
         // \n makes the next line go down. Put it at the end of the string.
 
@@ -94,7 +95,7 @@ class CodingChallenges()
         Console.WriteLine("Please input a number.");
         var checklessthan0 = Console.ReadLine();
         int checklessthan0int = int.Parse(checklessthan0);
-        Console.WriteLine("The statement " + checklessthan0int + " is less than or equal to 0 is " + lessThanOrEqualToZero(checklessthan0int) + "\n\n");
+        Console.WriteLine("The statement " + checklessthan0int + " is less than or equal to 0 is " + lessThanOrEqualToZero(checklessthan0int) + "\n\n"); */
     }
 
     public static int Sum (int a, int b)
@@ -157,18 +158,60 @@ class CodingChallenges()
             "Sum", "MinuteConvert", "AddOne", "FindPower", "YearsToDays", "IsLeapYear", "TriangleArea", "LessThanOrEqualToZero"
         };
 
-        int functionNumber;
-
-        foreach (string function in functions)
-        {
-            Console.WriteLine(functions[functionNumber]);
-            functionNumber++;
-        }
         
+
+        for (int i = 0; i < functions.Length; i++)
+        {
+            Console.WriteLine((i+1) + ". " + functions[i]);
+        }
+
+        var chosenFunction = Console.ReadLine();
+        int chosenFunctionInt =  int.Parse(chosenFunction);
+        chosenFunctionInt -= 1;
+        
+        Console.WriteLine("You have chosen " + functions[chosenFunctionInt]);
+
+        if (chosenFunctionInt == 0)
+        {
+            SumFunction();
+        }
     }
 
     public static void SumFunction ()
     {
+        Console.WriteLine("We are going to use the number adder called SUM. Please input two numbers for us to add. \n");
+        // \n makes the next line go down. Put it at the end of the string.
 
+        bool firstNumberInputted = false;
+
+        var number1 = Console.ReadLine(); // Console.ReadLine lets the user input a command.
+        int number1int;
+        if (int.TryParse(number1, out number1int))
+        {
+            firstNumberInputted = true;
+            number1int = int.Parse(number1); // int.Parse changes the variable # that the user inputs into a stored integer.
+        }
+        else
+        {
+            Console.WriteLine("Error: Please input a number.");
+
+        }
+        Console.WriteLine("Now please input a second number.");
+
+        var number2 = Console.ReadLine();
+        int number2int = int.Parse(number2);
+
+        Console.WriteLine("The sum of the number " + number1 + " and the number " + number2 + " is equal to: " + Sum(number1int, number2int) + "\n\n");
+    }
+
+    public static void MinuteConversion ()
+    {
+        Console.WriteLine("Now we are going to use the Minute Conversion system. It will find how many seconds are in minutes. \n");
+        Console.WriteLine("Please input a number for minutes.");
+
+        var minuteCount = Console.ReadLine();
+        int minutesInt = int.Parse(minuteCount);
+
+        Console.WriteLine("The number of seconds inside " + minutesInt + " minutes is " + MinuteConvert(minutesInt) + "\n\n");
     }
 }
